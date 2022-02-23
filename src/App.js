bfa4a2583235758d8903017bb6137307f5134809
global-Toba-dean <global-tobaogundimu@gmail.com>
2022-02-23 05:25:09 +0100

    starting to take shape :)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom'
+import { Layout } from 'antd';
+
+import { Cryptocurrencies, CryptoDetails, Exchanges, Homepage, Navbar, News, Footer } from './components'
+
 function App() {
   return (
-    <div className="App">
-      my crypto app begins!
+    <div className="app">
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path='/'>
+                <Homepage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
