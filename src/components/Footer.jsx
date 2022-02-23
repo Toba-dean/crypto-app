a1d1144bc69deae725f0aa7a97215dc33309e10f
global-Toba-dean <global-tobaogundimu@gmail.com>
2022-02-23 05:46:23 +0100

    done with nav bar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Typography, Space } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+        Copyright &copy; 2022
+        <Link to='/'> CryptoHub Inc.</Link> <br />
+        <Link to='/'>All Rights Reserved.</Link>
+      </Typography.Title>
+      <Space>
+        <Link to='/home'>Home</Link>
+        <Link to='/exchanges'>Exchanges</Link>
+        <Link to='/news'>News</Link>
+      </Space>
+    </>
   )
 }
 
